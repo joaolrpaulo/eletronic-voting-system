db4ef7584020c78710a16c216520b7470aaaa7fd
Nuno Azevedo <nunoaz95@gmail.com>
2017-04-29 04:32:58 +0100

    Configure port to run on https
    Signed-off-by: Nuno Azevedo <nunoaz95@gmail.com>

--- voting-server/run.py
+++ voting-server/run.py
@@ -3,5 +3,9 @@
 from app import app
 
 
-app.run(host = '0.0.0.0', port = 8000, debug = True,
-        ssl_context = ('certs/voting-server.crt', 'certs/voting-server.key'))
+app.run(
+    host = '0.0.0.0',
+    port = 443,
+    debug = True,
+    ssl_context = ('certs/voting-server.crt', 'certs/voting-server.key')
+)
