8782612f94fcb0bd526135bf7e1d354f49251440
Nuno Azevedo <nunoaz95@gmail.com>
2017-04-29 04:32:20 +0100

    Add hash and salt to database schema
    Signed-off-by: Nuno Azevedo <nunoaz95@gmail.com>

--- voting-server/database/schema.sql
+++ voting-server/database/schema.sql
@@ -1,6 +1,7 @@
 CREATE TABLE IF NOT EXISTS voters(
-    national_id INTEGER PRIMARY KEY NOT NULL,
-    username TEXT NOT NULL,
+    voter_id INTEGER PRIMARY KEY NOT NULL,
+    hash TEXT NOT NULL,
+    salt TEXT NOT NULL,
     name TEXT NOT NULL,
     email TEXT NOT NULL,
     address TEXT NOT NULL
