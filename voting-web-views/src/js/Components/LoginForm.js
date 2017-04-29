29e2e4a88478705453c5ba237ebafac7fa06801c
Miguel Oliveira <jmsoliv95@gmail.com>
2017-04-29 18:08:43 +0100

    some tweaks on main page

--- voting-web-views/src/js/Components/LoginForm.js
+++ voting-web-views/src/js/Components/LoginForm.js
@@ -17,27 +17,33 @@ export default class RegisterForm extends React.Component {
         return this.clicked;
     }
 
+    formSubmit (ev) {
+        const username = document.getElementById('username').value;
+        const password = document.getElementById('password').value;
+        console.log(username, password);
+    }
+
     render () {
         return (
             <div class="card wow fadeInRight" data-wow-duration="2.5s">
                 <div class="card-block">
                     <div class="text-center">
-                        <h3><i class="fa fa-user"></i> Login:</h3>
+                        <h3>Login:</h3>
                         <hr/>
                     </div>
                     <div class="md-form">
                         <i class="fa fa-user prefix"></i>
-                        <input type="text" id="form3" class="form-control"/>
+                        <input type="text" id="username" class="form-control"/>
                         <label for="form3">Your username</label>
                     </div>
                     <div class="md-form">
                         <i class="fa fa-lock prefix"></i>
-                        <input type="password" id="form4" class="form-control"/>
+                        <input type="password" id="password" class="form-control"/>
                         <label for="form4">Your password</label>
                     </div>
 
                     <div class="text-center">
-                        <button class="btn btn-primary btn-rounded btn-lg">Login</button>
+                        <button onClick={this.formSubmit.bind(this)} class="btn btn-primary btn-rounded btn-lg">Login</button>
                         <br/>
                         <br/>
                         <fieldset class="form-group">
