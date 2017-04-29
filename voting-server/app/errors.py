5d3e8d7bcd553a1e60803cf7b72c6ed1eaa5ba0b
Nuno Azevedo <nunoaz95@gmail.com>
2017-04-29 04:31:03 +0100

    Optimize imports
    Signed-off-by: Nuno Azevedo <nunoaz95@gmail.com>

--- voting-server/app/errors.py
+++ voting-server/app/errors.py
@@ -1,11 +1,11 @@
 from app import app
 from flask import jsonify
-from werkzeug.exceptions import HTTPException, default_exceptions
+from werkzeug.exceptions import HTTPException
+from werkzeug.exceptions import default_exceptions
 
 
 def json_error(error):
     response = jsonify(message = str(error))
-    print(str(error))
     response.status_code = error.code if isinstance(error, HTTPException) else 500
     return response
 
