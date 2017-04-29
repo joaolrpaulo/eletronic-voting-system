02276e5d0aa4ef17bd3dcb7b66a85d8eba7dd6a8
Nuno Azevedo <nunoaz95@gmail.com>
2017-04-29 04:31:23 +0100

    Add add/validators.py
    Signed-off-by: Nuno Azevedo <nunoaz95@gmail.com>

---
+++ voting-server/app/validators.py
@@ -0,0 +1,23 @@
+def voter_id(data):
+    # TODO: regex verification
+    return True
+
+
+def password(data):
+    # TODO: regex verification
+    return True
+
+
+def name(data):
+    # TODO: regex verification
+    return True
+
+
+def email(data):
+    # TODO: regex verification
+    return True
+
+
+def address(data):
+    # TODO: regex verification
+    return True
