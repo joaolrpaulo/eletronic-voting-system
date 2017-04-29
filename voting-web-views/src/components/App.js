3feba68032882556740a212794d5724675a23e7b
João Paulo <joaolrpaulo@gmail.com>
2017-04-30 00:40:42 +0100

    voting-web-views: added initial setup
    Signed-off-by: João Paulo <joao.paulo@feedzai.com>

---
+++ voting-web-views/src/components/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from '../styles/App.css';
+
+const App = () => (
+    <div className={styles.app}>
+        <h2>Que top</h2>
+    </div>
+);
+
+export default App;
