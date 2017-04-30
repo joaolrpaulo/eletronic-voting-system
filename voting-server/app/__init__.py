8c64e999c1adeca59e545c5e8751c9dc5207ecdd
Nuno Azevedo <nunoaz95@gmail.com>
2017-04-30 23:09:15 +0100

    voting-server: setup database with executescript
    Signed-off-by: Nuno Azevedo <nunoaz95@gmail.com>

--- voting-server/app/__init__.py
+++ voting-server/app/__init__.py
@@ -1,29 +1,27 @@
+import sys
+import sqlite3
 from flask import Flask
 from sqlalchemy import create_engine
+
+from app import configs
 
 
 # Initialize the app
 app = Flask(__name__)
-
-# Remove sorted objects
 app.config['JSON_SORT_KEYS'] = False
 
 
+# Parse app configs
+config = configs.parser(sys.argv[1])
+
+
 # Connect to database
-db = create_engine('sqlite:///database/voting-server.db')
+db = create_engine('sqlite:///' + config.database.location)
 
-# Setup voters table
-with open('database/voters.sql', mode = 'r') as schema, db.begin() as conn:
-    conn.execute(schema.read())
-
-# Setup tokens table
-with open('database/tokens.sql', mode = 'r') as schema, db.begin() as conn:
-    conn.execute(schema.read())
-
-
-# Fetch JWT secret
-with open('certs/secret.jwt', mode = 'r') as secret:
-    jwt_secret = secret.read()
+# Setup database
+with open(config.database.schema, mode = 'r') as schema, sqlite3.connect(config.database.location) as conn:
+    cursor = conn.cursor()
+    cursor.executescript(schema.read())
 
 
 # noinspection PyUnresolvedReferences
