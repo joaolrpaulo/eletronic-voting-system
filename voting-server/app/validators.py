3b59b6d3768cc922413e2d320660e6236f70c769
Nuno Azevedo <nunoaz95@gmail.com>
2017-04-30 21:41:20 +0100

    voting-server: fix wrong arguments order
    Signed-off-by: Nuno Azevedo <nunoaz95@gmail.com>

--- voting-server/app/validators.py
+++ voting-server/app/validators.py
@@ -3,24 +3,24 @@ import re
 
 def voter_id(data):
     # Exactly 8 numbers to follow National Card ID
-    return bool(re.match(str(data), '^(\d{8})$'))
+    return bool(re.match('^(\d{8})$', str(data)))
 
 
 def password(data):
     # Minimum 8 chars, 1 upper, 1 lower, 1 symbol, 1 numeric
-    return bool(re.match(data, '^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$'))
+    return bool(re.match('^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$', str(data)))
 
 
 def name(data):
     # Portuguese names
-    return bool(re.match(data, '^[A-Za-zÀ-ú]{1,20}( [A-Za-zÀ-ú]{1,20}){1,20}$'))
+    return bool(re.match('^[A-Za-zÀ-ú]{1,20}( [A-Za-zÀ-ú]{1,20}){1,20}$', str(data)))
 
 
 def email(data):
     # General email
-    return bool(re.match(data, '(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)'))
+    return bool(re.match('(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)', str(data)))
 
 
 def city(data):
     # Portuguese cities
-    return bool(re.match(data, '^[A-Za-zÀ-ú]+( [A-Za-zÀ-ú]+)*$'))
+    return bool(re.match('^[A-Za-zÀ-ú]+( [A-Za-zÀ-ú]+)*$', str(data)))
