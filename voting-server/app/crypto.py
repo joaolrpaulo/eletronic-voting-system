912f9fcad3f8f637de35f3263e8bc59d0c5562e9
Nuno Azevedo <nunoaz95@gmail.com>
2017-04-30 23:20:22 +0100

    voting-server: remove optional arguments
    Signed-off-by: Nuno Azevedo <nunoaz95@gmail.com>

--- voting-server/app/crypto.py
+++ voting-server/app/crypto.py
@@ -1,16 +1,13 @@
 from jose import jwt
 
-TTL = 300
-
 
 class Encryption:
     def __init__(self, secret):
-        self.secret = secret
+        with open(secret, mode = 'r') as f:
+            self.secret = f.read()
 
-    def encrypt(self, data = {}, algorithm = 'HS256'):
+    def encrypt(self, data, algorithm = 'HS256'):
         return jwt.encode(data, self.secret, algorithm = algorithm)
 
-    def decrypt(self, token = None, algorithm = ['HS256']):
-        if not token:
-            raise Exception('No data to decrypt.')
+    def decrypt(self, token, algorithm = ['HS256']):
         return jwt.decode(token, self.secret, algorithms = algorithm)
