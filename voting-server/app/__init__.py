46ba19302a3352b6f07245ce33a2ac2e3b3a1800
Nuno Azevedo <nunoaz95@gmail.com>
2017-04-30 17:32:32 +0100

    Optimize imports
    Signed-off-by: Nuno Azevedo <nunoaz95@gmail.com>

--- voting-server/app/__init__.py
+++ voting-server/app/__init__.py
@@ -1,32 +1,37 @@
 from flask import Flask
-from sqlalchemy import create_engine
 from flask_cors import CORS
+from sqlalchemy import create_engine
 
 
-# Initialize the App
+# Initialize the app
 app = Flask(__name__)
 
 # Remove sorted objects
 app.config['JSON_SORT_KEYS'] = False
 
-# CORS Support
+# CORS support
 app.config['CORS_HEADERS'] = 'Content-Type'
 cors = CORS(app)
 
-# Connects to DB
+
+# Connect to database
 db = create_engine('sqlite:///database/voting-server.db')
 
-# Initialize Voters table
+# Setup voters table
 with open('database/voters.sql', mode = 'r') as schema, db.begin() as conn:
     conn.execute(schema.read())
 
-# Initialize Tokens table
-with open('database/tokens.sql', mode='r') as schema, db.begin() as conn:
+# Setup tokens table
+with open('database/tokens.sql', mode = 'r') as schema, db.begin() as conn:
     conn.execute(schema.read())
 
-with open('certs/secret.jwt', mode='r') as secret:
-    secretContext = secret.read()
+
+# Fetch JWT secret
+with open('certs/secret.jwt', mode = 'r') as secret:
+    jwt_secret = secret.read()
+
 
 # noinspection PyUnresolvedReferences
 from app import errors
+# noinspection PyUnresolvedReferences
 from app import views
