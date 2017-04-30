72c11b918a4dbc71dca37b6a55391b91ee48a819
Nuno Azevedo <nunoaz95@gmail.com>
2017-04-30 23:19:43 +0100

    voting-server: remove database/voters.sql
    Signed-off-by: Nuno Azevedo <nunoaz95@gmail.com>

--- voting-server/database/voters.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE IF NOT EXISTS voters(
-    voter_id INTEGER PRIMARY KEY NOT NULL,
-    hash TEXT NOT NULL,
-    name TEXT NOT NULL,
-    email TEXT NOT NULL,
-    city TEXT NOT NULL
-);
