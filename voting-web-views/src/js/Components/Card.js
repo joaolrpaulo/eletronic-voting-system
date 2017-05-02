37ffa166452cc0a21e681c2039f72b24fe02fffe
Marco Pontes <marcopontes17@gmail.com>
2017-05-02 18:12:56 +0100

    voting-web-views: Design of login and voting pages

--- voting-web-views/src/js/Components/Card.js
+++ voting-web-views/src/js/Components/Card.js
@@ -12,9 +12,64 @@ export default class Card extends React.Component {
                 </div>
 
                 <div class="card-block">
-                    <h4 class="card-title">Card title</h4>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    <a href="#" class="btn btn-primary">Button</a>
+                    <h4 class="card-title">
+                        <p class="poll-name" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">Eleições Democráticas</p>
+                    </h4>
+                    <div class="collapse" id="collapseExample">
+                        <div class="card card-block scrollable">
+                            Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proidentsdcsdcdsjkncdjkns dcnjksdnkjdsilcds. kjnk. Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proidentsdcsdcdsjkncdjkns dcnjksdnkjdsilcds.
+                        </div>
+                    </div>
+                    <br/>
+                    <button type="button" class="btn btn-primary waves-effect waves-light" data-toggle="modal" data-target="#fullHeightModalRight">Large Modal</button>
+                    <div class="modal fade right" id="fullHeightModalRight" role="dialog" tabindex="-1" aria-labelledby="myModalLabel" aria-hidden="true">
+                        <div class="modal-dialog modal-full-height modal-right" role="document">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                        <span aria-hidden="true">×</span>
+                                    </button>
+                                    <h4 class="modal-title w-100" id="myModalLabel">Modal title</h4>
+                                </div>
+                                <div class="modal-body">
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente magnam, sunt, ea dolore eum quod. Minima fugiat enim aut soluta est reprehenderit reiciendis quos, qui, amet possimus laborum assumenda voluptate.
+                                    </p>
+
+                                    <ul class="list-group z-depth-0">
+                                        <li class="list-group-item justify-content-between">
+                                            Cras justo odio
+                                            <span class="badge badge-primary badge-pill">14</span>
+                                        </li>
+                                        <li class="list-group-item justify-content-between">
+                                            Dapibus ac facilisis in
+                                            <span class="badge badge-primary badge-pill">2</span>
+                                        </li>
+                                        <li class="list-group-item justify-content-between">
+                                            Morbi leo risus
+                                            <span class="badge badge-primary badge-pill">1</span>
+                                        </li>
+                                        <li class="list-group-item justify-content-between">
+                                            Cras justo odio
+                                            <span class="badge badge-primary badge-pill">14</span>
+                                        </li>
+                                        <li class="list-group-item justify-content-between">
+                                            Dapibus ac facilisis in
+                                            <span class="badge badge-primary badge-pill">2</span>
+                                        </li>
+                                        <li class="list-group-item justify-content-between">
+                                            Morbi leo risus
+                                            <span class="badge badge-primary badge-pill">1</span>
+                                        </li>
+                                    </ul>
+
+                                </div>
+                                <div class="modal-footer">
+                                    <button type="button" class="btn btn-secondary waves-effect waves-light" data-dismiss="modal">Close</button>
+                                    <button type="button" class="btn btn-primary waves-effect waves-light">Save changes</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         );
