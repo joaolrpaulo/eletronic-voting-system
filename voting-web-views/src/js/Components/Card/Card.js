a75434a4f5e27556b9b4c4abeefb56c67f5f402e
Miguel Oliveira <jmsoliv95@gmail.com>
2017-05-03 14:39:33 +0100

    voting-web-views: remove dummy request

--- voting-web-views/src/js/Components/Card/Card.js
+++ voting-web-views/src/js/Components/Card/Card.js
@@ -3,16 +3,6 @@ import React from 'react';
 import { axiosMethods } from 'Utils/configs.js';
 import CardTemplate from './Card.rt';
 
-export default class Card extends React.Component {
-    request () {
-        axiosMethods.get('/user')
-             .then((response) => {
-                 console.log(response);
-             })
-             .catch((error) => {
-                 console.log(error);
-             });
-    }
-}
+export default class Card extends React.Component {}
 
 Card.prototype.render = CardTemplate;
