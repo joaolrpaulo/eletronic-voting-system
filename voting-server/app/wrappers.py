14886ded3a4ce27d5f880fa6ead68dea291b3b46
Nuno Azevedo <nunoaz95@gmail.com>
2017-05-04 03:19:30 +0100

    voting-server: rename wrappers
    Signed-off-by: Nuno Azevedo <nunoaz95@gmail.com>

--- voting-server/app/wrappers.py
+++ voting-server/app/wrappers.py
@@ -1,43 +1,51 @@
 from flask import request, jsonify
+from jose import jwt
 
 from app import models, config
 from app.crypto import JWT
 
 
-def authorization(func):
-    def authorize(*args, **kwargs):
-        token = request.headers.get('Authorization').split('Bearer ')[1]
-        token_decrypted = JWT(config.jwt.secret).decrypt(token)
-        voter_id = token_decrypted.get('voter_id')
+def restricted(func):
+    def decorator(*args, **kwargs):
+        token = request.headers.get('Authorization')
+        if token and token.startswith('Bearer '):
+            try:
+                payload = JWT(config.jwt.secret).decrypt(token.replace('Bearer ', ''))
+                voter_id = payload.get('voter_id')
+                voter = models.VotersDB.get(voter_id)
+                if voter and voter.role == 'admin':
+                    return func(*args, **kwargs)
+                return jsonify({
+                    'message': 'permission denied'
+                }), 403
+            except (jwt.JWSError, jwt.ExpiredSignatureError) as e:
+                return jsonify({
+                    'message': str(e)
+                }), 401
+        return jsonify({
+            'message': 'token not provided'
+        }), 401
+    return decorator
 
-        if voter_id is None:
-            return jsonify(token_decrypted), 401
 
-        role = models.VotersDB().get(voter_id).role
-        path = request.path.split('/')[1][:-1]
-
-        if role != 'admin' and request.method == 'POST':
-            return jsonify({
-                'message': 'No permissions to create a new %s' % path
-            }), 401
-        return func(*args, **kwargs)
-    return authorize
-
-
-def authentication(func):
-    def authenticate(*args, **kwargs):
-        token = request.headers.get('Authorization').split('Bearer ')[1]
-        token_decrypted = JWT(config.jwt.secret).decrypt(token)
-        voter_id = token_decrypted.get('voter_id')
-
-        if voter_id is None:
-            return jsonify(token_decrypted), 401
-
-        voter = models.VotersDB().get(voter_id)
-
-        if not voter:
-            return jsonify({
-                'message': 'No such user found, please try another way to break the system.'
-            }), 401
-        return func(*args, **kwargs)
-    return authenticate
+def login_required(func):
+    def decorator(*args, **kwargs):
+        token = request.headers.get('Authorization')
+        if token and token.startswith('Bearer '):
+            try:
+                payload = JWT(config.jwt.secret).decrypt(token.replace('Bearer ', ''))
+                voter_id = payload.get('voter_id')
+                voter = models.VotersDB.get(voter_id)
+                if voter:
+                    return func(*args, **kwargs)
+                return jsonify({
+                    'message': 'permission denied'
+                }), 403
+            except (jwt.JWSError, jwt.ExpiredSignatureError) as e:
+                return jsonify({
+                    'message': str(e)
+                }), 401
+        return jsonify({
+            'message': 'token not provided'
+        }), 401
+    return decorator
