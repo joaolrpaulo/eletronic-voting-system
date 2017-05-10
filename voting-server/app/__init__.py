d63daf3db8e900d158c01eb417bb6d142e3c2eda
João Paulo <joaolrpaulo@gmail.com>
2017-05-11 00:46:48 +0100

    voting-server: implemented secure session system, and deprecated some old unused code.
    Signed-off-by: João Paulo <joaolrpaulo@gmail.com>

--- voting-server/app/__init__.py
+++ voting-server/app/__init__.py
@@ -8,17 +8,22 @@ from flask_cors import CORS
 from app import configs
 
 
-# Initialize the app
-app = Flask(__name__)
-app.config['JSON_SORT_KEYS'] = False
-CORS(app)
-
 # Parse app configs
 config = configs.parser(sys.argv[1])
 
 
+# Initialize the app
+app = Flask(__name__)
+app.config['JSON_SORT_KEYS'] = False
+app.config['SECRET_KEY'] = config.session.secret
+app.config['PERMANENT_SESSION_LIFETIME'] = config.session.ttl
+app.config['SESSION_COOKIE_SECURE'] = True
+CORS(app)
+
+
 # Connect to database
 db = create_engine('sqlite:///' + config.database.location)
+
 
 # Setup database
 with open(config.database.schema, mode = 'r') as schema, sqlite3.connect(config.database.location) as conn:
@@ -30,5 +35,3 @@ with open(config.database.schema, mode = 'r') as schema, sqlite3.connect(config.
 from app import errors
 # noinspection PyUnresolvedReferences
 from app import views
-# noinspection PyUnresolverReferences
-from app import schedulers
